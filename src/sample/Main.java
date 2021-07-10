f2860a9b0395aa728b309b797ddb780c8206f231
Kevin Stradtman <kevin.stradtman@gmail.com>
2021-07-10 17:50:35 -0400

    Added some of the sup project

--- src/sample/Main.java
+++ src/sample/Main.java
@@ -2,27 +2,32 @@ package sample;
 
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
-import javafx.scene.Group;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
-import javafx.scene.control.Button;
-import javafx.scene.layout.VBox;
-import javafx.scene.text.Text;
 import javafx.stage.Stage;
 
 public class Main extends Application {
 
     @Override
     public void start(Stage primaryStage) throws Exception{
-        //Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
-        Group root = new Group();
+        Parent root = FXMLLoader.load(getClass().getResource("/fxml/sample.fxml"));
+        /*Group root = new Group();
         Text txt = new Text("Sup?");
+        txt.setFont(new Font("Papyrus", 80));
+        Label label = new Label("Name:");
+        GridPane gridPane = new GridPane();
+        TextField textField = new TextField();
         Button btn = new Button();
+        gridPane.add(label, 0, 0);
+        gridPane.add(textField, 1, 0);
+        gridPane.add(btn, 1, 1);
+        gridPane.setHgap(20);
         txt.setY(50);
         btn.setText("Say Sup!");
+        btn.setOnAction(evt -> System.out.printf("Sup %s!%n", textField.getText()));
         VBox box = new VBox();
-        box.getChildren().addAll(txt, btn);
-        root.getChildren().add(box);
+        box.getChildren().addAll(txt, gridPane);
+        root.getChildren().add(box);*/
         primaryStage.setTitle("Sup");
         primaryStage.setScene(new Scene(root, 300, 275));
         primaryStage.show();
